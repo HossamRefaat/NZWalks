59a6f480899b469ef5fbbf21b2a2d786147ce04d
Hossam Refaat <hossamrefaat.dev@gmail.com>
2025-03-10 05:06:13 +0200

    Initial commit

---
+++ NZWalks.API/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            string[] names = new string[] { "Tom", "Jerry", "Mickey" };
+            return Ok(names);
+        }
+    }
+}
